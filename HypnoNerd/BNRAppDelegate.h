d65a13a2ba9bfc90c61cca38aea2c73adfc6b569
Jan Graßegger <jan@anycook.de>
2014-04-09 14:11:59 +0200

    Initial Commit

---
+++ HypnoNerd/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  HypnoNerd
+//
+//  Created by Jan Grassegger on 09.04.14.
+//  Copyright (c) 2014 Jan Graßegger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
